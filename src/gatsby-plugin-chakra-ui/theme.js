486aae048a1d3e39acaa30b88b46ee90fa81b655
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-10-03 11:08:52 -0700

    first

---
+++ src/gatsby-plugin-chakra-ui/theme.js
@@ -0,0 +1,11 @@
+import { theme } from "@chakra-ui/core"
+
+export default {
+  ...theme,
+  fonts: {
+    heading: "Josefin Sans, sans-serif",
+    body: "Josefin Sans, system-ui, sans-serif",
+    mono: "Menlo, monospace",
+  },
+  margin: 0,
+}
